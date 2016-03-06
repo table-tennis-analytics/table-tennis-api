bf67bde86f3790177d10b0d4459308108df4eefe
simonasdev <simonasgildutis@gmail.com>
2016-03-06 16:09:42 +0200

    Position

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :total_games, :total_points_scored, :total_points_lost, :total_wins, :total_losses, :coefficient, :position
 
-  def include_position?
-    object.rated?
-  end
-
   def coefficient
     object.coefficient.round
   end
